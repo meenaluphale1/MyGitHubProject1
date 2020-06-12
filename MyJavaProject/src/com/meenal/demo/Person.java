3361a7004d58c34b997d295eeea095a899e37aa0
MeenalAnand <meenalanand@git.com>
2020-06-12 18:56:06 +0530

    First commit of My Java Project, with  2 java files Person.java and Client.java

---
+++ MyJavaProject/src/com/meenal/demo/Person.java
@@ -0,0 +1,43 @@
+package com.meenal.demo;
+
+public class Person {
+	
+private String personName;
+private Integer personAge;
+private Double personScore;
+public Person() {
+	super();
+}
+public Person(String personName, Integer personAge, Double personScore) {
+	super();
+	this.personName = personName;
+	this.personAge = personAge;
+	this.personScore = personScore;
+}
+public String getPersonName() {
+	return personName;
+}
+public void setPersonName(String personName) {
+	this.personName = personName;
+}
+public Integer getPersonAge() {
+	return personAge;
+}
+public void setPersonAge(Integer personAge) {
+	this.personAge = personAge;
+}
+public Double getPersonScore() {
+	return personScore;
+}
+public void setPersonScore(Double personScore) {
+	this.personScore = personScore;
+}
+@Override
+public String toString() {
+	return "Person [personName=" + personName + ", personAge=" + personAge + ", personScore=" + personScore + "]";
+}
+
+
+
+
+}
